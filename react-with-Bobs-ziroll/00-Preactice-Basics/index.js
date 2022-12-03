4fbfa240620aa2f9d272f551bfa924462d52365c
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-03 18:15:36 +0530

    quiz.md added

--- react-with-Bobs-ziroll/00-Preactice-Basics/index.js
+++ react-with-Bobs-ziroll/00-Preactice-Basics/index.js
@@ -78,10 +78,7 @@ ReactDOM.render(Val, document.getElementById("root"));
       - Inside the 'ul' have three 'li
   for "Priceing" , "About" , and "Contact"
   
-  
-  */
-
-    const nav = (
+   const nav = (
  <nav>
  <h1>Hello React.com</h1>
  <ul>
@@ -93,3 +90,72 @@ ReactDOM.render(Val, document.getElementById("root"));
     )
 
     ReactDOM.render(nav , document.getElementById("root"));
+  */
+
+
+ /*
+
+Challenge 8: find out what happens if we try to append JSX to our div#root using.append() instead of ReactDOM
+1. Create a sample page in JSX(≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append your JSX
+3. See if you can guess what will show up in the browser before running the code
+4. See if you can explain what actually shows up in the browser
+
+
+const Page = (
+    <div>
+        <h1>Hello React.com</h1>
+        <h2>Hello React.com2</h2>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Page</li>
+        </ul>
+        </div>
+
+)
+// it returns the object . JSX is return plain object to the dom and ReactDOM compile it and convert Browser undstandable code
+
+// const root = document.getElementById("root").append(Page) //[Oject][Object] is output
+
+const root = document.getElementById("root").append(JSON.stringify(Page))
+
+/* output object in string form :
+{"type":"div","key":null,"ref":null,"props":{"children":[{"type":"h1","key":null,"ref":null,"props":{"children":"Hello React.com"},"_owner":null,"_store":{}},{"type":"h2","key":null,"ref":null,"props":{"children":"Hello React.com2"},"_owner":null,"_store":{}},{"type":"ul","key":null,"ref":null,"props":{"children":[{"type":"li","key":null,"ref":null,"props":{"children":"Menu"},"_owner":null,"_store":{}},{"type":"li","key":null,"ref":null,"props":{"children":"About"},"_owner":null,"_store":{}},{"type":"li","key":null,"ref":null,"props":{"children":"Page"},"_owner":null,"_store":{}}]},"_owner":null,"_store":{}}]},"_owner":null,"_store":{}
+*/
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Challenge 9: Starting from scratch, build and render the HTML for our section project. Check the Google slide for what you're trying to build.
+We'll be adding styling to it later.
+Hints:
+  *The React logo is a file in the project tree, so you can access it by using `src="./react-logo.png" in your image element
+ * You can also set the 'width` attribute of the image element just like in HTML. In the slide, I have it set to 40px 
+
+  */
+
+  const project = (
+      <div>
+       <div className ="logo">
+              <img src="../1-task-active/logo.png" width ="40%px" />
+       </div>
+     <h1 className>Fun fact about React</h1>
+    <div className ="text">
+              <ul>
+                  <li>Was first released in 2013</li>
+                  <li>Was orginally creadted by Jorden Walke</li>
+                  <li>Is maintainde by Facebook</li>
+                  <li>Powers thousands of enterprise aoos, including mobile apps</li>
+                  <li>Has well over 100k stars on GitHub</li>
+              </ul>
+    </div>
+        
+      </div>
+       )
+       ReactDOM.render(project , document.getElementById("root"))
+
+
+
+
