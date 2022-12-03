a8d35465f4bf595c373fa1576898eb68114092cf
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-03 20:20:19 +0530

    quiz 2 added

--- react-with-Bobs-ziroll/00-Preactice-Basics/index.js
+++ react-with-Bobs-ziroll/00-Preactice-Basics/index.js
@@ -134,9 +134,8 @@ Hints:
   *The React logo is a file in the project tree, so you can access it by using `src="./react-logo.png" in your image element
  * You can also set the 'width` attribute of the image element just like in HTML. In the slide, I have it set to 40px 
 
-  */
 
-  const project = (
+ const project = (
       <div>
        <div className ="logo">
               <img src="../1-task-active/logo.png" width ="40%px" />
@@ -155,7 +154,67 @@ Hints:
       </div>
        )
        ReactDOM.render(project , document.getElementById("root"))
+  */
 
 
 
+
+/**
+Challenge 9: =>
+Part 1: Create a page of your own using a custom Page component
+It should return an ordered list with the reasons why you're excited to be learning React :)
+Render your list to the page
+
+function MyPage(){
+return(
+
+    <React.Fragment>
+       
+       <ol>
+        <li>I wanna something from my life</li>
+        <li>I don't want be use less</li>
+        <li>I wanna good software engineer</li>
+        <li>I wanna good job as well </li>
+       </ol>
+
+    </React.Fragment>
+)
+}
+
+ReactDOM.render(<MyPage/>, document.getElementById("root"))
+
+
+*/
+/*
+Challenge 10: =>
+Add a header` element with a nested `nav` element. 
+Inside the `nav` include a `img` element with the image of the React logo inside (src="./react-logo.png") and make sure to set the width to something more manageable so it doesn't take up the whole screen
+Add an `h1` with some text describing the page. (E.g. "Reasons I'm excited to learn React"). Place it above the ordered list. Add a footer after the list that says:
+"20xx <last name here> development. All rights reserved."
+
+
+function WebPage(){
+  return(
+    <React.Fragment>
+        <header>
+            <nav>
+                  <img src="../1-task-active/logo.png"/>
+            </nav>
+        </header>
+          <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+              <li>I wanna something from my life</li>
+              <li>I don't want be use less</li>
+              <li>I wanna good software engineer</li>
+              <li>I wanna good job as well </li>
+        </ol>
+          <h3> &copy; 2022 Mehra development. All rights reserved.</h3>
+      </React.Fragment>
+  )
+}
+
+
+ReactDOM.render(<WebPage />, document.getElementById("root"))
+
+*/
 
