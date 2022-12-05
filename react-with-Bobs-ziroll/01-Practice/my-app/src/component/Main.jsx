832f4049f9b2cc137a7cabbed3eb6ec78b688446
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-05 19:15:45 +0530

    task 1 complete

--- react-with-Bobs-ziroll/01-Practice/my-app/src/component/Main.jsx
+++ react-with-Bobs-ziroll/01-Practice/my-app/src/component/Main.jsx
@@ -5,7 +5,7 @@ export default function Main(){
         <div className="main">
           <h1 className="main-heading">Fun facts about React</h1>
 
-          <div className="main-content">
+         
             <ul className="main-list">
               <li>Was first released in 2013</li>
               <li>was originally created by Jorden Walke</li>
@@ -13,12 +13,8 @@ export default function Main(){
               <li>Is maintained by Facebook</li>
               <li>Powers thousands of enterprise app, including mobile app</li>
             </ul>
-            <img
-              src={require("./reactjs-icon 2.png")}
-              alt="img"
-              className="main--icon"
-            ></img>
-          </div>
+            
+          
         </div>
       </>
     );
