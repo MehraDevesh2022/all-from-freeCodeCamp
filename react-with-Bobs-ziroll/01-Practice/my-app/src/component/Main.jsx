d554ebc8864cf1ed31903685b425eb42902a0510
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-05 18:42:00 +0530

    main css added

--- react-with-Bobs-ziroll/01-Practice/my-app/src/component/Main.jsx
+++ react-with-Bobs-ziroll/01-Practice/my-app/src/component/Main.jsx
@@ -3,15 +3,22 @@ export default function Main(){
     return (
       <>
         <div className="main">
-          <h1 className="main-heading">Fun facts about Recat</h1>
+          <h1 className="main-heading">Fun facts about React</h1>
 
-          <ul className="main-list">
-            <li>Was first relase in 2013</li>
-            <li>was originally created by Jorden Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise app, including mobile app</li>
-          </ul>
+          <div className="main-content">
+            <ul className="main-list">
+              <li>Was first released in 2013</li>
+              <li>was originally created by Jorden Walke</li>
+              <li>Has well over 100k stars on Github</li>
+              <li>Is maintained by Facebook</li>
+              <li>Powers thousands of enterprise app, including mobile app</li>
+            </ul>
+            <img
+              src={require("./reactjs-icon 2.png")}
+              alt="img"
+              className="main--icon"
+            ></img>
+          </div>
         </div>
       </>
     );
