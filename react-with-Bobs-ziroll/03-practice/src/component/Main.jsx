52155feac117ff97e00154b72b2e16ac90e99521
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-17 23:14:37 +0530

    update

--- react-with-Bobs-ziroll/03-practice/src/component/Main.jsx
+++ react-with-Bobs-ziroll/03-practice/src/component/Main.jsx
@@ -1,27 +1,29 @@
 import memeData from "./memeData";
-
+import React from "react";
 export default function Main() {
-  const clickHandle = () => {
-    let Size = memeData.data.memes.length;
+  
+  let [imgUrl, setImgUrl] = React.useState("https://i.imgflip.com/1w7ygt.jpg");
+  const clickHandleImage = () => {
+   let Size = memeData.data.memes.length;
     let val = Math.trunc(Math.random() * Size) + 1;
-          console.log(memeData.data.memes[val].url);
-      
-    
+    console.log(memeData.data.memes[val].url);
+         setImgUrl(prvUrl => (prvUrl= memeData.data.memes[val].url));
   };
   return (
     <>
       <main>
-        <form className="from">
+      {/* <form className="from">
           <input type="text" placeholder="top text" className="form--input" />
           <input
             type="text"
             placeholder="bottom text"
             className="form--input"
           />
-        </form>
-        <button className="form--button" onClick={clickHandle}>
+        </form> */}
+        <button className="form--button" onClick={clickHandleImage}>
           Get a new meme image 🖼
         </button>
+        <img src ={imgUrl} alt="" className="main-img" />
       </main>
     </>
   );
