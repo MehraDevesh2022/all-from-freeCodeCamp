07b2a7b2107de634c78d3875d735a3a26c517413
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-26 20:59:49 +0530

    winner scire added

--- react-with-Bobs-ziroll/tenzies-game/src/component/Die.jsx
+++ react-with-Bobs-ziroll/tenzies-game/src/component/Die.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Die(props) {
-   console.log(props.isFalse);
+  //  console.log(props.isFalse);
     const myStyle = {
       backgroundColor: props.isFalse ? "#59E391" : "white",
     };
