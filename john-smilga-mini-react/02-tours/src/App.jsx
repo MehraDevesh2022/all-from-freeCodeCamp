9fdeaef5274c5996c03754600680739aa43acc0f
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-27 17:33:23 +0530

    02- tours added

--- john-smilga-mini-react/02-tours/src/App.jsx
+++ john-smilga-mini-react/02-tours/src/App.jsx
@@ -4,64 +4,56 @@ import Loading from "./component/Loding";
 import Tours from "./component/Tours";
 
 function App() {
-  const  [loading , setLoading]=  useState(true)
+  const [loading, setLoading] = useState(true);
   const [tourData, setTourData] = useState([]);
 
-function removeCard(id){
-  const newData = tourData.filter(data =>{
-    return data.id !=  id
-  })
-  setTourData(prvData => [...newData])
-}
+  function removeCard(id) {
+    const newData = tourData.filter((data) => {
+      return data.id != id;
+    });
+    setTourData((prvData) => [...newData]);
+  }
 
-function fetchApi (){
-    setLoading(true)
+  function fetchApi() {
+    setLoading(true);
     fetch("https://course-api.com/react-tours-project")
       .then((res) => res.json())
-      .then((data) =>{
-         setTourData([...data]);
-         setLoading(false);
-      })
-    
-    
-      
-}
+      .then((data) => {
+        setTourData([...data]);
+        setLoading(false);
+      });
+  }
 
   useEffect(function () {
-  fetchApi();
-      
-  },[]);
- 
- 
-       if(loading === true){
-         return(
-          <Loading/>
-         )
-       }
+    fetchApi();
+  }, []);
 
-         if(tourData.length ==0){
-          return (
-            <main>
-              <div className="title">
-                <h2>no tours left</h2>
-                <button onClick={() =>fetchApi() } className ="btn">refresh</button>
-              </div>
-            </main>
-          );
-          }
-          return(
-            <main>
 
-              <Tours 
-              tours ={tourData}
-              deleteHandler ={removeCard}
-              />
-            </main>
-          )
-         
-       }
-  
-      
- 
+
+  // when all cards are wiped out(delted) and fecthing or in inital state when card array ==0
+  if (loading === true) {
+    return <Loading />;
+  }
+
+ // when all cards are deleted then show button to refrece again( load cards )
+  if (tourData.length == 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button onClick={() => fetchApi()} className="btn">
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  // when all other condition fails then show cards
+  return (
+    <main>
+      <Tours tours={tourData} deleteHandler={removeCard} />
+    </main>
+  );
+}
 
 export default App;
