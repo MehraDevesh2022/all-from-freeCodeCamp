e2c55d2db4bcfc70bcb9a34929616281c8ebbda0
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-28 17:25:08 +0530

    react icon added

--- john-smilga-mini-react/03-review-project/src/component/Review.jsx
+++ john-smilga-mini-react/03-review-project/src/component/Review.jsx
@@ -1,32 +1,36 @@
-export default function Review(props) {
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
-let { id, name, job, image, text } = props.userArr;
-console.log(props);
-// console.log(id);
-//  console.log(name);
-//  console.log(job);
-//  console.log(image);
-//  console.log(text);
-    return (
-      <article className="review">
-        <div className="img-container">
-          <img src={image} alt="img" className="person-img" />
-          <span className="quote-icon"></span>
-        </div>
-        <h4 className="author">{name}</h4>
-        <div className="job">{job}</div>
-        <div className="info">{text}</div>
-        <div className="button-container">
-          <button className="prev-btn" onClick={props.handlePrev}>
-            pv
-          </button>
-          <button className="next-btn" onClick={props.handleNext}>
-            nx
-          </button>
-        </div>
-        <button className="random-btn" onClick={props.randomUser}>
-          Surprise me
+export default function Review(props) {
+  let { id, name, job, image, text } = props.userArr;
+  console.log(props);
+  // console.log(id);
+  //  console.log(name);
+  //  console.log(job);
+  //  console.log(image);
+  //  console.log(text);
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt="img" className="person-img" />
+        <span className="quote-icon">
+          {" "}
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <div className="job">{job}</div>
+      <div className="info">{text}</div>
+      <div className="button-container">
+        <button className="prev-btn" onClick={props.handlePrev}>
+          <FaChevronLeft />
         </button>
-      </article>
-    );
+        <button className="next-btn" onClick={props.handleNext}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={props.randomUser}>
+        Surprise me
+      </button>
+    </article>
+  );
 }
