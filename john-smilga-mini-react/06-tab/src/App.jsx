eb158247b54f80b4dd3d4dbbdc7718b073d624b8
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-12-31 12:58:11 +0530

    05 tabs

--- john-smilga-mini-react/06-tab/src/App.jsx
+++ john-smilga-mini-react/06-tab/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./component/style.css";
+import { FaAngleDoubleRight } from "react-icons/fa";
 let url = "https://course-api.com/react-tabs-project";
 
 function App() {
@@ -20,53 +21,65 @@ function App() {
     }
   }
 
+
+  // side effect
   React.useEffect(() => {
     fetchData();
   }, []);
 
-  //  while data is fething
+  //  while data is fething loading.. will shows
   if (loading) {
-    return <h2>loading...</h2>;
+  return (
+    <section className="section loading">
+      <h1>Loading...</h1>
+    </section>
+  );
   }
+
+
+
 
   return (
     <section className="section">
+      {/* heading */}
       <div className="title">
         <h2>Experience</h2>
         <div className="underline"></div>
       </div>
 
-      {/* job buttons */}
-      <div className="job-center">
+      <div className="jobs-center">
+        {/* job buttons */}
         <div className="btn-container">
-           {data.map((job ,index) =>{
+          {data.map((job, index) => {
             // console.log(job);
-                 return (
-                   <button
-                   key={job.id}
-                     className={`job-btn ${index ===jobIdx && 'acive-btn' }`}
-                     onClick={() => setJobIdx(index)}
-                   >
-                     {job.company}
-                   </button>
-                 );
-           })}
-         
+            return (
+              <button
+                key={job.id}
+                onClick={() => setJobIdx(index)}
+                className={`job-btn ${index === jobIdx && "active-btn"}`}
+              >
+                {job.company}
+              </button>
+            );
+          })}
         </div>
         {/* job-info */}
         <article className="job-info">
-          <h3>title</h3>
-          <h4>company</h4>
-          <p className="job-date">Date</p>
-          <div className="job-desc">
-            <div className="job-icon">{"==>"}</div>
-            <p>duty</p>
-            <p>duty</p>
-            <p>duty</p>
-          </div>
+          <h3>{data[jobIdx].title}</h3>
+          <h4>{data[jobIdx].company}</h4>
+          <p className="job-date">{data[jobIdx].dates}</p>
+           
+             {/* job desc */}
+            {data[jobIdx].duties.map((duite, index) => {
+              return( <div key={index} className="job-desc">
+                 <FaAngleDoubleRight className="job-icon" />
+                 <p>{duite}</p>
+               </div>)
+            })}
+          
         </article>
       </div>
-
+       {/* more btn */}
       <button type=" button" className="btn">
         more info
       </button>
