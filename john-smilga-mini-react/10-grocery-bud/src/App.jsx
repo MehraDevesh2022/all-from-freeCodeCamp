71d5d1f8377e61212aa1900f955535d0e126ae47
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-01-04 17:30:16 +0530

    groscery bud added

--- john-smilga-mini-react/10-grocery-bud/src/App.jsx
+++ john-smilga-mini-react/10-grocery-bud/src/App.jsx
@@ -2,84 +2,70 @@ import React  ,{useEffect , useState} from "react";
 import List from "./component/List";
 import Alert from "./component/Alert";
 import "./component/style.css"
-function App() {
-   
-  function getLocalStorageData(){
-    let mylist  =  JSON.parse(localStorage.getItem('list'));
-       
-    if(mylist){
-      return(mylist)
-    }else{
+ function getLocalStorageData() {
+   let mylist = JSON.parse(localStorage.getItem("list"));
+
+   if (mylist) {
+     return mylist;
+   } else {
      return [];
-    }
-  }
+   }
+ }
 
+function App() {
+  const [name, setName] = useState("");
+  const [alert, setAlert] = useState({ show: false, msg: "", color: "" });
+  const [list, setList] = useState(getLocalStorageData());
+  const [isEditiing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
 
-  const [name , setName] = useState("")
-  const [alert , setAlert] = useState({show : false , msg : "" , color : ""})
-  const [list  , setList] = useState(getLocalStorageData());
-  const [isEditiing , setIsEditing] = useState(false);
-  const [editId , setEditId] = useState(null);
-  
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
-  
-    useEffect(() => {
-      localStorage.setItem("list", JSON.stringify(list));
-    }, [list]);  
-       // showAlert With default values if there is no parameter passed by user then autmatically deafult parameter will set
-  function showAlert(show = "false" , msg ="" , color =""){
-    console.log(show);
-    setAlert({show : show , msg : msg , color :color});
-  }
-
-  function handleSubmit(e){
-   e.preventDefault();
-   if(!name){
- // when name is empty in the from then setAlert
-showAlert(true , "text is empty" , "danger")
-    }
-    else if(name && isEditiing){
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name) {
+      // when name is empty in the from then setAlert
+      showAlert(true, "text is empty", "danger");
+    } else if (name && isEditiing) {
       // edit the list item
       setList(
-        list.map(item =>{
-          if(item.id === editId){
-            return {...item , title : name}
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
           }
           return item;
         })
-      )
+      );
       // now set name empty
-      setName('');
+      setName("");
       // isEditing state now false then submit button will visible instead of edit in form
       setIsEditing(false);
       // id set null bceuse given id item values changed
-      setEditId(null)
+      setEditId(null);
       // set alert
-      showAlert(true , "item is modified successfully !.. " , "success")
-
-    }
-    else{
-        //setAlert for new item added and add the item into the list
-        const newItem = {id : new Date().getTime().toString() , title : name};
-        // set new item into the list
-        setList([...list , newItem]);
-        // set name empty
-         setName('')
-         // set alert
-         showAlert(true , "item added successfully ..." , "success")
+      showAlert(true, "item is modified successfully !.. ", "success");
+    } else {
+        showAlert(true, "item added successfully ...", "success");
+      //setAlert for new item added and add the item into the list
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      // set new item into the list
+      setList([...list, newItem]);
+      // set name empty
+      setName("");
+      // set alert
+    
     }
   }
 
-
   // edit listItem first find then setName with that item and change state isEditing , setEdit
   function ediItem(id) {
-      let currEditItem = list.find(item => item.id ==id);
-      console.log(currEditItem);
-      setName(currEditItem.title);
-      setEditId(id);
-      setIsEditing(true);
-
-
+    let currEditItem = list.find((item) => item.id == id);
+    console.log(currEditItem);
+    setName(currEditItem.title);
+    setEditId(id);
+    setIsEditing(true);
   }
 
   function clearList() {
@@ -91,15 +77,17 @@ showAlert(true , "text is empty" , "danger")
 
   function removeItem(id) {
     // filter always return new array ...
-    setList( 
-      list.filter(item => item.id !=id) 
-     )
-     // set the alert as well
-       showAlert(true , 'item removed from list' , "danger");
-
+    setList(list.filter((item) => item.id != id));
+    // set the alert as well
+    showAlert(true, "item removed from list", "danger");
   }
-  
-  
+  // showAlert With default values if there is no parameter passed by user then autmatically deafult parameter will set
+
+  function showAlert(show = false, msg = "", color = "") {
+    console.log("showAlert" , show);
+    setAlert({ show, msg, color });
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
--- john-smilga-mini-react/10-grocery-bud/src/component/Alert.jsx
+++ john-smilga-mini-react/10-grocery-bud/src/component/Alert.jsx
@@ -1,11 +1,13 @@
 import React , {useEffect }from 'react'
 
 function Alert({ msg, color, removeAlert, list }) {
+  console.log("alert");
   useEffect(() => {
     const timeout = setTimeout(() => {
+      console.log("from setTimeOut");
       removeAlert();
     }, 3000);
-    return () => clearTimeout(timeout);
+    // return () => clearTimeout(timeout);
   }, [list]);
 
   return <p className={`alert alert-${color}`}>{msg}</p>;
